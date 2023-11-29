0177da4301e1ca4fbdf70ac266036013d61ae5fb
oncarrozaqy <oncarrozaqyy@gmail.com>
2023-11-29 19:36:27 +0700

    fix: change router regist

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
-from datetime import datetime, timedelta
+from datetime import timedelta
 from ..database import cursor
 from ..oauth2 import create_access_token, authenticate_user, ACCESS_TOKEN_EXPIRE_MINUTES
-from ..utils import verify_password
+from ..utils import hash_password
+from ..database import cursor, conn
 
 auth_router = APIRouter(
     prefix='/authentications',
@@ -21,3 +22,41 @@ async def login(login: Annotated[OAuth2PasswordRequestForm, Depends()]):
         access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
         access_token = create_access_token(data={"user": user[6]}, expires_delta=access_token_expires)
         return {"access_token": access_token, "token_type": "bearer"}
+    
+@auth_router.post('/register')
+async def register_user(firstname: str, lastname: str, phonenumber: str, address: str, email: str, password: str, username: str, role: str):
+    if (len(username) <= 3):
+        raise HTTPException(status_code=405, detail="Username must be at least 4 characters!")
+        return
+    
+    if (len(password) <= 5):
+        raise HTTPException(status_code=405, detail="Password must be at least 6 characters!")
+        return
+    
+    query = ("SELECT * FROM users")
+    cursor.execute(query)
+    result = cursor.fetchall()
+    if not result :
+        userid = 1
+    else :
+        query = ("SELECT MAX(userid) FROM users")
+        cursor.execute(query)
+        result = cursor.fetchall()
+        userid = result[0][0] + 1
+
+    query = ("SELECT * FROM users WHERE email = %s")
+    cursor.execute(query, (email,))
+    result = cursor.fetchall()
+    if result :
+        return "Email unavailable! Try Another One!"
+    else :
+        query = ("SELECT * FROM users WHERE username = %s")
+        cursor.execute(query, (username,))
+        result = cursor.fetchall()
+        if result :
+            return "Username unavailable! Try Another One!"
+        else :
+            query = ("INSERT INTO users (userID, firstName, lastName, email, phoneNumber, shippingAddress, username, password , role) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)")
+            cursor.execute(query, (userid, firstname, lastname, phonenumber, address, email, username, hash_password(password), role))
+            conn.commit()
+            return "Registration Completed!"
